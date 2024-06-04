19dbcf8ae0a45784f5f444abdd26f470581c945c
Utkarsh Karki <utkarsh1998@icloud.com>
2024-06-04 12:09:42 -0700

    Bottom Nav Bar Composable

--- app/src/main/java/com/example/jetpackcomposepractice2/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractice2/MainActivity.kt
@@ -4,22 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Face
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposepractice2.ui.theme.JetpackComposePractice2Theme
 
 class MainActivity : ComponentActivity() {
@@ -37,40 +57,100 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
+    val items = listOf(
+        NavigationItemState(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            hasBadge = false,
+            badgeNumber = 0
+        ),
+        NavigationItemState(
+            title = "Inbox",
+            selectedIcon = Icons.Filled.Email,
+            unselectedIcon = Icons.Outlined.Email,
+            hasBadge = true,
+            badgeNumber = 10
+        ),
+        NavigationItemState(
+            title = "Account",
+            selectedIcon = Icons.Filled.Face,
+            unselectedIcon = Icons.Outlined.Face,
+            hasBadge = true,
+            badgeNumber = 0
+        )
+    )
+    var bottomNavState by rememberSaveable {
+        mutableIntStateOf(0)
+    }
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "Scaffold Top App Bar")
-                }
-            )
-        },
         bottomBar = {
-            BottomAppBar {
-                Text(
-                    text = "Scaffold Bottom App Bar"
-                )
-            }
-        },
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = { /*TODO*/ }
-            ) {
-                Icon(imageVector = Icons.Filled.Add, contentDescription = "Add")
+            NavigationBar {
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = bottomNavState == index,
+                        onClick = {
+                            bottomNavState = index
+                        },
+                        icon = {
+                            BadgedBox(
+                                badge = {
+                                    if(item.hasBadge) {
+                                        Badge {}
+                                    }
+                                    if(item.badgeNumber != 0) {
+                                        Badge {
+                                            Text(
+                                                text = item.badgeNumber.toString()
+                                            )
+                                        }
+                                    }
+                                }
+                            ) {
+                                    Icon(
+                                        imageVector =
+                                            if(bottomNavState == index)
+                                                item.selectedIcon
+                                            else
+                                                item.unselectedIcon,
+                                        contentDescription = item.title
+                                    )
+                            }
+                        },
+                        label = {
+                            Text(
+                                text = item.title
+                            )
+                        }
+                    )
+                }
+
             }
         }
     ) { innerPadding ->
         Column(
             modifier
                 .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "content"
+                text = items[bottomNavState].title,
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 22.sp
             )
         }
     }
 }
 
+data class NavigationItemState(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasBadge: Boolean,
+    val badgeNumber: Int
+)
 
 @Preview(showBackground = true)
 @Composable
